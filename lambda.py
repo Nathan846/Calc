13faca947b78df8a9f9ef475e1f7ba94b5dda432
Nathan846 <54005418+Nathan846@users.noreply.github.com>
2021-02-11 14:37:02 +0530

    Update lambda.py

--- lambda.py
+++ lambda.py
@@ -2,5 +2,9 @@ from calculator import Calculator
 def handler(event,handler):
     x = event['x']
     y = event['y']
-    print('Addition of two numbers is ',Calculator.add(x,y))
+    calc = Calculator()
+    print('Addition of two numbers is ',calc.add(x,y))
+    print('Subraction of two numbers is ',calc.sub(x,y))
+    print('Multiplication of two numbers is ',calc.mul(x,y))
+    print('Division of two numbers is ',calc.div(x,y))
 
